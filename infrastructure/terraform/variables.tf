cd3d08e5337d1315f73acd61b87d699f373bff0d
madhavi <madhavi.ruwandika@gmail.com>
2024-09-06 13:43:34 +1000

    fix deploy step iissue

---
+++ infrastructure/terraform/variables.tf
@@ -0,0 +1,8 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "aws_account_id" {}
+variable "ecr_repository" {}
+variable "key_name" {}
