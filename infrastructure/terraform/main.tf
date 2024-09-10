f0d2af874bfb7ccd7b2cd238a819a6c7d4c7b7cd
madhavi <madhavi.ruwandika@gmail.com>
2024-09-10 21:58:53 +1000

    remove ipv6

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -95,7 +95,7 @@ resource "aws_security_group" "allow_ssh_http" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"] 
   }
 }
 
